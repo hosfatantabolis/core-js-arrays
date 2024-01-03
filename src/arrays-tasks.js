51fbdd329082cead2b87a262835c3e17b373f466
Maksim Litvinov <lmk478shade@gmail.com>
2024-01-03 22:13:18 +0300

    feat: calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -327,8 +327,12 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr
+    .map((item) => {
+      return item[0] - item[1];
+    })
+    .reduce((acc, item) => acc + item, 0);
 }
 
 /**

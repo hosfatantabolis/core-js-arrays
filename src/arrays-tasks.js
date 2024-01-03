00dc3df8f93d1c9ec5ead062b59e6e74d5cc8152
Maksim Litvinov <lmk478shade@gmail.com>
2024-01-03 22:43:01 +0300

    feat: createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -347,8 +347,13 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((acc, item, index) => {
+    const chunkNumber = Math.floor(index / chunkSize);
+    if (!acc[chunkNumber]) acc[chunkNumber] = [];
+    acc[chunkNumber].push(item);
+    return acc;
+  }, []);
 }
 
 /**
